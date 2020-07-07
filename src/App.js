aba4ca9b97084b8088932b883562ea373d30579e
Joel Clifford <joel.clifford@icloud.com>
2020-07-06 21:03:33 -0400

    removed data seeding function

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import Wrapper from "./components/Wrapper/index"
 import Title from "./components/Title/index"
 import Table from "./components/Table"
 
-import makeEmployees from "./utils/makeEmployees"
-
 function App() {
-
-  const [employees, useEmployees] = useState(makeEmployees(5));
 
   return (
     <Wrapper>
       <Title title="Employees"/>
-      <Table data={employees}/>
+      <Table />
     </Wrapper>
   )
 };
