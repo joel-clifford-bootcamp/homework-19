41886a38315faec6f8b548afbab16954dd32563f
Joel Clifford <joel.clifford@icloud.com>
2020-07-09 22:47:20 -0400

    moved employee data generation to App.js

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,63 +3,13 @@ import "./style.css";
 import TableHeader from "../TableHeader"
 import TableBody from "../TableBody"
 
-import makeEmployees from "../../utils/makeEmployees"
-
-class Table extends React.Component {
-
-  employees = makeEmployees(5)
-  sorts = [];
-  filters = {};
-
-  state = {
-    employees: this.employees
-  };
-
-  filterColumns = (fieldName, filterValue) => {
-
-    if(filterValue.length === 0){
-      if(this.filters.hasKey(fieldName))
-        delete this.filters[fieldName];
-    }
-    else{
-      this.filters[fieldName] = filterValue
-    }
-
-    let employeesCopy = [...this.employees];
-
-    for(var key in this.filters){
-      employeesCopy = employeesCopy.filter(employee => {
-        employee[key].toString().toLowerCase().includes(filterValue.toLowerCase());
-      })
-    }
-
-    this.setState({employees: employeesCopy});
-  }
-
-  mapBySorts = (sortDefinition) => {
-    let employeesCopy = [...this.employees]
-
-    let currentDefinition = this.sorts.filter(item => item.fieldName === sortDefinition.fieldName);
-
-    if(sortDefinition.direction === 0)
-      this.sorts.splice(this.sorts.indexOf(currentDefinition),1)
-    else
-      this.sorts[this.sorts.indexOf(currentDefinition)].direction = sortDefinition.direction; 
-      
-    this.sorts.forEach(sort => {
-      employeesCopy = employeesCopy
-    })
-    
-  }
-
-  render() {
+function Table(props){
     return (    
       <table className="table">
-        <TableHeader columnNames={["#", "First Name", "Last Name"]} filterColumns={this.filterColumns} />
-        <TableBody data={this.state.employees} />
+        <TableHeader columnNames={["#", "First Name", "Last Name"]} />
+        <TableBody data={props.data} />
       </table>
     )
-  };
 }
 
 export default Table;
