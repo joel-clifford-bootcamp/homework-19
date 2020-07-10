85d31a5dbf90ee5e8911af46ac4f7d803f1d85de
Joel Clifford <joel.clifford@icloud.com>
2020-07-09 22:43:04 -0400

    added dropdown for sort order selction

---
+++ src/components/Dropdown/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function Dropdown(props) {
+
+    return (   
+        
+        <div className="form-group">
+        <label>Last Name Sort Order</label>
+        <select className="form-control" onChange={props.change}>
+            <option>-</option>
+            <option>A - Z</option>
+            <option>Z - A</option>
+        </select>
+        </div>
+    )
+}
+
+export default Dropdown;
