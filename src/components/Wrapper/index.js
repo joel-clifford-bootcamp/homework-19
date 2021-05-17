6f499bec82b3b88c4d1ee706b9ed9c2912b43a3a
joel-clifford-bootcamp <59773595+joel-clifford-bootcamp@users.noreply.github.com>
2021-05-16 21:40:51 -0400

    Added ChangeEmployees modal form

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -2,7 +2,31 @@ import React from "react";
 import "./style.css";
 
 function Wrapper(props) {
-  return <div className="container-fluid">{props.children}</div>;
+  return <div className="container-fluid">
+    {props.children}
+    <div className="modal fade" id="modalForm" role="dialog">
+      <div className="modal-dialog modal-dialog-centered" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalCenterTitle">Change Employees Collection</h5>
+            <button type="button btn-md" className="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <form className="form-inline">
+              <label htmlFor="formControlRange">How Many Employees?</label>
+              <input type="range" className="form-control-range" id="formControlRange"></input>
+            </form>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" className="btn btn-primary">Update</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
 }
 
 export default Wrapper;
