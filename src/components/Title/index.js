b18fbaef43ca5e253cc6aaf94f705e609172a617
joel-clifford-bootcamp <59773595+joel-clifford-bootcamp@users.noreply.github.com>
2021-05-16 21:40:34 -0400

    Added button to open Change Employees modal

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./style.css";
 
 function Title(props) {
-  return <h1 className="jumbotron">{props.title}</h1>;
+  return <div className="jumbotron d-flex justify-content-between">
+    <h1>{props.title}</h1>
+    <button className="btn btn-primary align-self-center" data-toggle="modal" data-target="#modalForm">Change Employees</button>
+    </div>;
 }
 
 export default Title;
