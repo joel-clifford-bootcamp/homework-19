315170521a71712a9e575c25ba1782568e520a41
joel-clifford-bootcamp <59773595+joel-clifford-bootcamp@users.noreply.github.com>
2021-05-16 21:40:12 -0400

    Passed filtering and sorting props  to ColumnHeaders

--- src/components/TableHeader/index.js
+++ src/components/TableHeader/index.js
@@ -1,23 +1,25 @@
 import React from "react"
 import ColumnHeader from "../ColumnHeader"
 
-function getFieldName(columnName)
+function getColumnName(fieldName)
 {
-    let columnNameWords = columnName.split(' ');
+    let columnNameWords = fieldName.split(' ');
     columnNameWords[0] = columnNameWords[0].toLowerCase()
     return columnNameWords.join('');
 }
 
-
 function TableHeader(props){
     return <thead>
         <tr>
-            {props.columnNames.map(columnName => 
+            {props.columnNames.map(fieldName => 
                 <ColumnHeader 
-                    key={props.columnNames.indexOf(columnName)}
-                    columnName={columnName}
-                    fieldName={getFieldName(columnName)}
-                    filterColumns={props.filterColumns}/>
+                    key={props.columnNames.indexOf(fieldName)}
+                    columnName={getColumnName(fieldName)}
+                    fieldName={fieldName}
+                    updateCollection={props.updateCollection}
+                    sortDescription={props.sortDescription}
+                    sort={props.sort}
+                    filter={props.filter}/>
                 )}
         </tr>
     </thead>
