4943f2c67a3ffc6a24244b6055a0529db754e023
joel-clifford-bootcamp <59773595+joel-clifford-bootcamp@users.noreply.github.com>
2021-05-16 21:37:29 -0400

    Refactored to remove  filtering and sorting responsibilities

--- src/App.js
+++ src/App.js
@@ -1,91 +1,29 @@
-import React, {Component, useState} from "react";
+import React, {Component} from "react";
 import Wrapper from "./components/Wrapper/index"
 import Title from "./components/Title/index"
 import Table from "./components/Table"
-import Input from "./components/Input"
-import Dropdown from "./components/Dropdown"
-import Form from "./components/Form"
 
 import makeEmployees from "./utils/makeEmployees"
 
-class App extends React.Component { 
+class App extends Component { 
 
-  // Stores unfiltered and un-sorted employees list
   employees = [];
 
-  // Stores sort direction
-  sortDirection = 0;
-
   state = {
-    employees: []
+    collection: this.employees
   };
 
   // Populate list with employees when component mounts
   componentDidMount(){
-    this.employees = makeEmployees(5);
-    this.setState({employees: this.employees});
-  }
-
-  // set sortDirection and then setState to sortedList
-  handleDropdownChange = event => {
-
-    switch(event.target.value){
-      case 'A - Z':
-        this.sortDirection = 1;
-        break;
-      case "Z - A":
-        this.sortDirection = -1;
-        break;
-      default:
-        this.sortDirection = 0;
-        break
-    }
-
-    this.setState({employees: this.sortLastName(this.state.employees)});
-  }
-
-  // Sort employees by last name
-  sortLastName = (employeesList) => {
-
-    switch(this.sortDirection){
-      case 1:
-        return employeesList.sort((a, b) => (a.lastName > b.lastName) ? 1 : -1);
-      case -1:
-        return employeesList.sort((a, b) => (a.lastName > b.lastName) ? -1 : 1);
-      default:
-        return employeesList;
-    }
-  }
-
-  // filter list, sort result and then update the state
-  filterLastName = (text) => {
-    
-      if(text.length > 0){
-        const filteredEmployees = this.state.employees.filter(employee => employee.lastName.includes(text));
-        this.setState({employees: this.sortLastName(filteredEmployees)});
-      }
-      else{
-        this.setState({employees: this.sortLastName(this.employees)});
-      }
-  }
-
-  // Handle changes to the filter input
-  handleInputChange = event => {
-    const value = event.target.value;
-
-    this.filterLastName(value);
+    this.employees = makeEmployees(15);
+    this.setState( { collection: this.employees } );
   }
 
   render() {
-
     return (
       <Wrapper>
         <Title title="Employees"/>
-        <Form>
-          <Input change={this.handleInputChange}/>
-          <Dropdown change={this.handleDropdownChange}/>
-        </Form>
-        <Table data={this.state.employees}/>
+        <Table collection={this.state.collection} changeCollection={this.setState}/>
       </Wrapper> 
     )
   }
