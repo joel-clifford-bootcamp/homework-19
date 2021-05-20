9478e47e4af99f9ac9588f70362523b43f52904c
joel-clifford-bootcamp <59773595+joel-clifford-bootcamp@users.noreply.github.com>
2021-05-20 12:45:58 -0400

    Replaced implementation w/ react-table filtering

--- src/components/FilterDropdown/index.js
+++ src/components/FilterDropdown/index.js
@@ -1,43 +1,36 @@
-import React, {useState} from "react";
+import React from "react";
 import "./style.css";
 
-function FilterDropDown(props) {
+function FilterDropDown({column}) {
   
-  let filterString = ""
+  // let filterString = ""
   let isFilterStringEmpty = true;
-  const getFilterIconClass = () =>  isFilterStringEmpty ? "fas fa-filter empty" : "fas fa-filter";
+  const filterIconClass = isFilterStringEmpty ? "fas fa-filter empty" : "fas fa-filter";
 
-  const [InputValue, setInputValue] = useState("");
-  const [filterIconClass, setFilterIconClass] = useState(getFilterIconClass());
-  const [isClearButtonDisabled, setIsClearButtonDisabled] = useState(true);
+  // const [InputValue, setInputValue] = useState("");
+  // const [filterIconClass, setFilterIconClass] = useState(getFilterIconClass());
+  // const [isClearButtonDisabled, setIsClearButtonDisabled] = useState(true);
   
-  const applyFilter = () => props.filter({fieldName: props.fieldName, filterString: filterString});
-  const setIsFilterStringEmpty = () => isFilterStringEmpty = filterString === "";
+  // const applyFilter = () => props.filter({fieldName: props.fieldName, filterString: filterString});
+  // const setIsFilterStringEmpty = () => isFilterStringEmpty = filterString === "";
   
-  const setFilterString = newFilterString => { 
-    filterString = newFilterString;
-    setIsFilterStringEmpty();
-    setInputValue(newFilterString);
-    setFilterIconClass(getFilterIconClass());
-    setIsClearButtonDisabled(isFilterStringEmpty);
-    applyFilter();
-  }
+  // const setFilterString = newFilterString => { 
+  //   filterString = newFilterString;
+  //   setIsFilterStringEmpty();
+  //   setInputValue(newFilterString);
+  //   setFilterIconClass(getFilterIconClass());
+  //   setIsClearButtonDisabled(isFilterStringEmpty);
+  //   applyFilter();
+  // }
 
-  const onInputChange = event => setFilterString(event.target.value);
+  // const onInputChange = event => setFilterString(event.target.value);
   
-  return <span className="dropdown">
+  return <span className="dropdown" aria-expanded="true">
       <button className="btn btn-sm" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      <span className={filterIconClass}></span>
+      <span className={column.filterValue ? "fas fa-filter" : "fas fa-filter empty" }></span>
     </button>
     <div className="dropdown-menu">
-      <div className="d-flex">
-          <input className="form-control" 
-            type="text" 
-            placeholder="Filter by" 
-            value={InputValue}
-            onChange={onInputChange}></input>
-          <button className="btn" onClick={() => setFilterString("")} disabled={isClearButtonDisabled}>Clear</button>
-        </div>
+      {column.render("Filter")}
     </div>
   </span>
 }
